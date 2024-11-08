cd4145bf6e0814351dbbddb6a9a889d39559299d
lurejewel <lurejewel@outlook.com>
2024-11-08 14:48:21 +0800

    new function: output gait phase

---
+++ cal_gait_phase.m
@@ -0,0 +1,75 @@
+function [phaseR, phaseL] = cal_gait_phase(model, state, GRF)
+%% materials for gait phase detection
+% init gait phases
+phaseR = -1;
+phaseL = -1;
+
+% body weight, stance threshold
+BW = -model.getTotalMass(state) * model.getGravity.get(1); % body weight (N)
+stanceTh = 0.23137978; % for heel strike detection
+
+% Normal GRF (暂时先通过地反力文件读取，后期改为接触力反算的形式)
+grfR = GRF(1)/BW; % normal component of GRF, right-foot side
+grfL = GRF(2)/BW; % left-foot side
+
+% X-axis of pelvis COM
+pelvis = model.getBodySet().get('pelvis');
+pelvisCOMLocal = pelvis.getMassCenter();
+pelvisCOMGlobal = pelvis.findStationLocationInGround(state, pelvisCOMLocal);
+pelvisCOM = pelvisCOMGlobal.get(0);
+
+% X axis of calcn COM
+calcnL = model.getBodySet().get('calcn_l');
+calcnLCOMLocal = calcnL.getMassCenter();
+calcnLCOMGlobal = calcnL.findStationLocationInGround(state, calcnLCOMLocal);
+calcnLCOM = calcnLCOMGlobal.get(0);
+calcnR = model.getBodySet().get('calcn_r');
+calcnRCOMLocal = calcnR.getMassCenter();
+calcnRCOMGlobal = calcnR.findStationLocationInGround(state, calcnRCOMLocal);
+calcnRCOM = calcnRCOMGlobal.get(0);
+
+%% gait detection
+% [Early Stance]: GRF >= stanceTh && calcnCOM >= pelvisCOM
+% [Late Stance]:  GRF >= stanceTh && calcnCOM < pelvisCOM && opposite GRF <
+% stanceTh
+% [Liftoff]:      GRF >= stanceTh && calcnCOM < pelvisCOM && (opposite GRF 
+% > stanceTh || calcnCOM+1 < pelvisCOM)
+% [Swing]:        GRF < stanceTh && calcnCOM < pelvisCOM
+% [Landing]:      GRF < stanceTh && calcnCOM >= pelvisCOM
+
+% right
+if grfR >= stanceTh
+    if calcnRCOM >= pelvisCOM
+        phaseR = 0; % Early Stance
+    elseif grfL > stanceTh || calcnRCOM+1 < pelvisCOM
+        phaseR = 2; % LiftOff
+    elseif grfL < stanceTh
+        phaseR = 1; % Late Stance
+    end
+elseif calcnRCOM < pelvisCOM
+    phaseR = 3; % Swing
+elseif calcnRCOM >= pelvisCOM
+    phaseR = 4; % Landing
+end
+
+% left
+if grfL >= stanceTh
+    if calcnLCOM >= pelvisCOM
+        phaseL = 0; % Early Stance
+    elseif grfR > stanceTh || calcnLCOM+1 < pelvisCOM
+        phaseL = 2; % LiftOff
+    elseif grfR < stanceTh
+        phaseL = 1; % Late Stance
+    end
+elseif calcnLCOM < pelvisCOM
+    phaseL = 3; % Swing
+elseif calcnLCOM >= pelvisCOM
+    phaseL = 4; % Landing
+end
+
+% check
+if phaseR == -1 || phaseL == -1
+    error('invalid gait phase detected.')
+end
+
+end
---
+++ determine_gait_phase_from_motion.m
@@ -0,0 +1,64 @@
+% TODO1 (Completed): 给模型加载上外力（subject01_walk1_grf.xml），读取足部的受力，判断步态阶段。
+% TODO2: 编写肌肉反射机制和优化算法。
+% TODO3: 保证能够通过读取接触力，正确且准确地实时获取地反力（正压力和摩擦力）。
+
+
+addpath('assets\', 'model\')
+import org.opensim.modeling.*
+
+%% load model
+model = Model('model\coupled_human-prosthesis_model_scaledFinal.osim');
+model.setUseVisualizer(true);
+state = model.initSystem();
+
+%% load motion and GRFdata
+motData = Storage('assets\subject01_walk1_ik.mot');
+rate = motData.getSize / (motData.getLastTime - motData.getFirstTime);
+phase = nan(motData.getSize, 2);
+
+motDataGRF = Storage('assets\subject01_walk1_grf.mot');
+
+%% determine gait phase
+for t = 0 : motData.getSize - 1 % for each frame/state
+    stateVec = motData.getStateVector(t).getData; % get state vector for the model at the frame
+    currentState = state.updQ(); % fetch the data array of the model at *t* state
+
+    grfR = motDataGRF.getStateVector(t*10).getData.get(1); % t*10: freq of grf data is 10 times higher than that of ik motion data
+    grfL = motDataGRF.getStateVector(t*10).getData.get(7);
+
+    % assign the state vector to the model
+    for i = 0 : stateVec.size - 1 % for each coordinate (DOF)
+        coordVal = stateVec.get(i);
+        if i<3 || i>5 % i=3~5 is translational dof, no need for conversion
+            coordVal = deg2rad(coordVal);
+        end
+        currentState.set(i, coordVal);
+    end
+    model.realizeDynamics(state);
+    % calculate gait phase based on the state of the model
+    [phase(t+1,1), phase(t+1,2)] = cal_gait_phase(model, state, [grfR, grfL]); % phaseR phaseL
+end
+
+% %% debug: plot the gait phase
+% timeVec = 0 : 1/rate : 2.5-1/rate;
+% figure, plot(timeVec, phase(:,1))
+% hold on, plot(timeVec, phase(:,2))
+% xlabel('t/s'); ylabel('gait phase'); legend('right leg', 'left leg')
+
+%% play the video
+for t = 0 : motData.getSize - 1 % for each frame/state
+    stateVec = motData.getStateVector(t).getData; % get state vector for the model at the frame
+    currentState = state.updQ(); % fetch the data array of the model at *t* state
+    % for each DOF, assign the state from motion file (stateVec) to the
+    % state of the model (currentState)
+    for i = 0 : stateVec.size - 1
+        coordVal = stateVec.get(i); 
+        if i<3 || i>5 % i=3~5 is translational dof, no need for conversion
+            coordVal = deg2rad(coordVal);
+        end
+        currentState.set(i, coordVal);
+        % model.realizeDynamics(state); % not necessary, but better do it
+    end
+    model.updVisualizer().show(state);
+    pause(1/rate)
+end
